c1a5b772a90500bc576c1fdd3daa206a747660cd
Mario Casas Donjuan <a01287116@tec.mx>
2024-04-15 02:07:34 -0600

    Package methods, Lista de comandos

---
+++ src/ListaDeComandos_Git.java
@@ -0,0 +1,7 @@
+
+public class ListaDeComandos_Git {
+	//para checar lo que falta de subir: git status
+	//para agregar las carpetas y cambios: git add .
+	//para hacer commit: git commit -m "mensaje para ver los cambios"
+	//para hacer push: git push
+}
