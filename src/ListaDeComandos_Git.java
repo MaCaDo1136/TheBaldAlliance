727d1a2140378fa5e3a27be9feba7b8165911676
Mario Casas Donjuan <a01287116@tec.mx>
2024-04-15 02:20:39 -0600

    prueba

--- src/ListaDeComandos_Git.java
+++ src/ListaDeComandos_Git.java
@@ -4,4 +4,5 @@ public class ListaDeComandos_Git {
 	//para agregar las carpetas y cambios: git add .
 	//para hacer commit: git commit -m "mensaje para ver los cambios"
 	//para hacer push: git push
+	//prueba
 }
